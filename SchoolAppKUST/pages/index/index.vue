fb0bd0c0e0099a8d95914248e46ddbcdd82a96b5
何向阳Rainbow <simpsonhxy@163.com>
2022-02-21 12:01:36 +0800

    首页修改

--- SchoolAppKUST/pages/index/index.vue
+++ SchoolAppKUST/pages/index/index.vue
@@ -3,18 +3,7 @@
 		<h1>校园首页</h1>
 		<!-- <view class="intro">本项目为：校园圈 小程序，计划寒假开发，参照需求文档。</view>
 		<text class="intro">计划：1-2月 完成基本的功能 </text><br>
-		<uni-link :href="href" :text="conentLink"></uni-link>
-		<view class="">
-		        <uni-forms :modelValue="formData">
-		            <uni-forms-item label="姓名" name="name">
-		                <uni-easyinput type="text" v-model="formData.name" placeholder="请输入姓名" />
-		            </uni-forms-item>
-		            <uni-forms-item required name="hobby" label="兴趣爱好">
-		                <uni-data-checkbox multiple v-model="formData.hobby" :localdata="hobby"/>
-		            </uni-forms-item>
-		        </uni-forms>
-		        <button @click="submitForm">Submit</button>
-		    </view> -->
+		<uni-link :href="href" :text="conentLink"></uni-link> -->
 		<!-- 首页轮播图 -->
 		<u-swiper :list="swiperList" indicator indicatorMode="line" circular keyName="image" showTitle></u-swiper>
 		<u-divider text="分割线" :dot="true"></u-divider>
@@ -26,7 +15,19 @@
 				</view>
 				<view class="album__content">
 					<u--text text="子敬" type="primary" bold size="17"></u--text>
-					<u--text margin="0 0 8px 0" text="考研复习资料"></u--text>
+					<u--text margin="0px 0 8px 0" text="考研复习资料"></u--text>
+					<u-album :urls="urls"></u-album>
+				</view>
+			</view>
+		</view>
+		<view class="u-demo-block__content">
+			<view class="album">
+				<view class="album__avatar">
+					<image src="/static/logo.png" mode="" style="width: 32px;height: 32px;"></image>
+				</view>
+				<view class="album__content">
+					<u--text text="艾弗森" type="primary" bold size="17"></u--text>
+					<u--text margin="0px 0 8px 0" text="考研复习资料"></u--text>
 					<u-album :urls="urls"></u-album>
 				</view>
 			</view>
@@ -87,5 +88,8 @@
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
+	}
+	.u-demo-block__content{
+		margin-top: 15rpx;
 	}
 </style>
